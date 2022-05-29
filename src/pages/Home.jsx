94e5d883e6f8646de67590173613057de048ba12
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-05-29 12:52:51 +0400

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div className="search-wrap">
+      <div>
+        <input placeholder="Search..." type="text" />
+        <button>Search</button>
+      </div>
+    </div>
+  );
+};
