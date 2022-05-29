01ee74d1a855c65d57febbb25f931ce7b3b5ed98
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-05-29 16:36:54 +0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,55 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import "./index.css";
 
 export const Home = () => {
+  const [response, setResponse] = useState([]);
+  const [name, setName] = useState("");
+  const [showComponent, setShowComponent] = useState(false);
+  const url = `https://api.github.com/search/repositories?q=${name}%20in:name&per_page=5`;
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        // handle success
+        setResponse(response.data.items);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  }, [url]);
+
+  const handleButton = (e) => {
+    e.preventDefault();
+    setShowComponent(!showComponent);
+    setName("");
+  };
+
+  const Data = () => {
+    return (
+      <ul>
+        {response.map((item) => {
+          return <li key={item.id}>{item.full_name}</li>;
+        })}
+      </ul>
+    );
+  };
+
   return (
     <div className="search-wrap">
       <div>
-        <input placeholder="Search..." type="text" />
-        <button>Search</button>
+        <input onChange={handleChange} placeholder="Search..." type="text" />
+        <button onClick={handleButton}>Search</button>
       </div>
+
+      <nav className="data">{showComponent ? <Data /> : null}</nav>
     </div>
   );
 };
