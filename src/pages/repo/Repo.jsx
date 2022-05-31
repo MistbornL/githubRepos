71f13fbc103131d40257abc8b0254bb84980249b
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-05-31 16:16:44 +0400

    ts

--- src/pages/repo/Repo.jsx
+++ src/pages/repo/Repo.jsx
@@ -1,3 +1,4 @@
+import { Alert, AlertTitle } from "@mui/material";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -10,6 +11,7 @@ export const Repo = () => {
   const params = useParams();
   const reduxData = useSelector((state) => state);
   const [repo, setRepo] = useState({ id: 0 });
+  console.log(repo);
   useEffect(() => {
     reduxData.responseData.map((item) => {
       if (item.id.toString() === params.id) {
@@ -19,9 +21,22 @@ export const Repo = () => {
     });
   }, [reduxData.responseData, params.id]);
 
-  return (
-    <div className="card-wrapper">
-      <BasicCard key={repo.id} repo={repo}></BasicCard>
-    </div>
-  );
+  if (repo.id !== 0) {
+    return (
+      <div className="card-wrapper">
+        <BasicCard key={repo.id} repo={repo} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="alert">
+        <Alert style={{ width: "500px" }} severity="error">
+          <AlertTitle>
+            <strong>Error</strong>
+          </AlertTitle>
+          not found
+        </Alert>
+      </div>
+    );
+  }
 };
